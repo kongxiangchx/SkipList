a2941711390f936fb47b299a1c229c1a377f643c
kongxiangchx <773046513@qq.com>
2023-01-13 01:17:39 +0800

    update test

--- tests/test_stl.cc
+++ tests/test_stl.cc
@@ -1,15 +1,72 @@
 #include "chx/skiplist.h"
 #include <functional>
+#include <cstring>
 #include <vector>
+#include <unordered_set>
+#include <set>
+#include <unordered_map>
 
-int main() {
+void test_string() {
+    chx::SkipList<std::string, std::string> skipList(6);
+	skipList.insertNode("name", "jack");
+    skipList.insertNode("age", "18");
+    skipList.dumpFile("../store/string.kv");
+    skipList.displayList();
+
+    chx::SkipList<std::string, std::string> skipList1(6);
+    skipList1.loadFile("../store/string.kv");
+    skipList1.displayList();
+}
+
+void test_vector() {
+    chx::SkipList<std::string, std::vector<std::string>> skipList(6);
+	skipList.insertNode("name", std::vector<std::string>{"jack", "rose"});
+    skipList.dumpFile("../store/vector.kv");
+    skipList.displayList();
+
+    chx::SkipList<std::string, std::vector<std::string>> skipList1(6);
+    skipList1.loadFile("../store/vector.kv");
+    skipList1.displayList();
+}
+
+void test_uset() {
+    chx::SkipList<std::string, std::unordered_set<std::string>> skipList(6);
+	skipList.insertNode("name", std::unordered_set<std::string>{"jack","rose"});
+    skipList.dumpFile("../store/uset.kv");
+    skipList.displayList();
+
+    chx::SkipList<std::string, std::unordered_set<std::string>> skipList1(6);
+    skipList1.loadFile("../store/uset.kv");
+    skipList1.displayList();
+}
+
+void test_set() {
+    chx::SkipList<std::string, std::set<std::string>> skipList(6);
+	skipList.insertNode("name", std::set<std::string>{"jack","rose"});
+    skipList.dumpFile("../store/set.kv");
+    skipList.displayList();
+
+    chx::SkipList<std::string, std::set<std::string>> skipList1(6);
+    skipList1.loadFile("../store/set.kv");
+    skipList1.displayList();
+}
+
+void test_umap() {
     chx::SkipList<std::string, std::unordered_map<std::string,std::string>> skipList(6);
 	skipList.insertNode("name", std::unordered_map<std::string,std::string>{{"jack","12"},{"rose","13"}});
-    skipList.dumpFile("../store/stl.kv");
+    skipList.dumpFile("../store/umap.kv");
     skipList.displayList();
 
     chx::SkipList<std::string, std::unordered_map<std::string,std::string>> skipList1(6);
-    skipList1.loadFile("../store/stl.kv");
+    skipList1.loadFile("../store/umap.kv");
     skipList1.displayList();
+}
+
+int main() {
+    test_string();
+    test_vector();
+    test_uset();
+    test_set();
+    test_umap();
     return 0;
 }
